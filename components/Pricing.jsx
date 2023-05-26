ceb94337077bbb2072ea60c7262d79381c35f314
arushiMWork2004 <133617549+arushiMWork2004@users.noreply.github.com>
2023-05-26 12:56:36 +1000

    Update Pricing.jsx

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -9,7 +9,7 @@ const PricingPlan = () => {
 
   return (
     <div className="flex justify-center items-center py-8">
-      <div className="max-w-lg">
+      <div className="max-w-5xl">
         <div className="flex justify-center mb-4">
           <button
             className={`px-4 py-2 mr-2 ${
@@ -28,7 +28,7 @@ const PricingPlan = () => {
             Yearly
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-8">
           <div className="bg-white p-8 rounded shadow w-full">
             <h2 className="text-2xl font-bold mb-4">Basic</h2>
             <p className="text-gray-600 mb-4">{isMonthly ? '$10/month' : '$100/year'}</p>
@@ -41,7 +41,7 @@ const PricingPlan = () => {
               Select Plan
             </button>
           </div>
-          <div className="bg-white p-8 rounded shadow w-full">
+          <div className="bg-white p-10 rounded shadow w-full">
             <h2 className="text-2xl font-bold mb-4">Standard</h2>
             <p className="text-gray-600 mb-4">{isMonthly ? '$20/month' : '$200/year'}</p>
             <ul className="text-gray-600">
@@ -49,7 +49,7 @@ const PricingPlan = () => {
               <li>Feature 2</li>
               <li>Feature 3</li>
             </ul>
-            <button className="bg-red-300 text-white py-2 px-4 mt-4 rounded">
+            <button className="bg-red-300 text-white py-2 px-4 mt-6 rounded">
               Select Plan
             </button>
           </div>
