c0219f00e2dbdf2f1976d17812df0b401205cdda
arushiMWork2004 <133617549+arushiMWork2004@users.noreply.github.com>
2023-05-29 09:51:47 +1000

    Update Pricing.jsx

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -8,8 +8,8 @@ const PricingPlan = () => {
   };
 
   return (
-    <div className="flex justify-center items-center py-8">
-      <div className="max-w-5xl">
+    <div className="grid justify-items-center py-8">
+      <div>
         <div className="flex justify-center mb-4">
           <button
             className={`px-4 py-2 mr-2 ${
@@ -28,10 +28,12 @@ const PricingPlan = () => {
             Yearly
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-8">
-          <div className="bg-white p-8 rounded shadow w-full">
-            <h2 className="text-2xl font-bold mb-4">Basic</h2>
-            <p className="text-gray-600 mb-4">{isMonthly ? '$10/month' : '$100/year'}</p>
+        <div className="grid grid-cols-1 gap-16 sm:grid-cols-3">
+          <div className="bg-white p-8 rounded shadow">
+            <h2 className="text-2xl font-bold font-mono mb-4">Basic</h2>
+            <p className="text-gray-600 mb-4">
+              {isMonthly ? '$10/month' : '$100/year'}
+            </p>
             <ul className="text-gray-600">
               <li>Feature 1</li>
               <li>Feature 2</li>
@@ -41,21 +43,11 @@ const PricingPlan = () => {
               Select Plan
             </button>
           </div>
-          <div className="bg-white p-10 rounded shadow w-full">
-            <h2 className="text-2xl font-bold mb-4">Standard</h2>
-            <p className="text-gray-600 mb-4">{isMonthly ? '$20/month' : '$200/year'}</p>
-            <ul className="text-gray-600">
-              <li>Feature 1</li>
-              <li>Feature 2</li>
-              <li>Feature 3</li>
-            </ul>
-            <button className="bg-red-300 text-white py-2 px-4 mt-6 rounded">
-              Select Plan
-            </button>
-          </div>
-          <div className="bg-white p-8 rounded shadow w-full">
-            <h2 className="text-2xl font-bold mb-4">Premium</h2>
-            <p className="text-gray-600 mb-4">{isMonthly ? '$30/month' : '$300/year'}</p>
+          <div className="bg-white p-8 rounded shadow">
+            <h2 className="text-2xl font-bold font-mono mb-4">Standard</h2>
+            <p className="text-gray-600 mb-4">
+              {isMonthly ? '$20/month' : '$200/year'}
+            </p>
             <ul className="text-gray-600">
               <li>Feature 1</li>
               <li>Feature 2</li>
@@ -64,6 +56,22 @@ const PricingPlan = () => {
             <button className="bg-red-300 text-white py-2 px-4 mt-4 rounded">
               Select Plan
             </button>
+          </div>
+          <div className="bg-white rounded shadow">
+            <div className="p-8">
+              <h2 className="text-2xl font-bold font-mono mb-4">Premium</h2>
+              <p className="text-gray-600 mb-4">
+                {isMonthly ? '$30/month' : '$300/year'}
+              </p>
+              <ul className="text-gray-600">
+                <li>Feature 1</li>
+                <li>Feature 2</li>
+                <li>Feature 3</li>
+              </ul>
+              <button className="bg-red-300 text-white py-2 px-4 mt-4 rounded">
+                Select Plan
+              </button>
+            </div>
           </div>
         </div>
       </div>
