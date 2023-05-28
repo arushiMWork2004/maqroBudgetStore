a667f5d6f3adc2473b696bb502d8a1eefea0e3fb
arushiMWork2004 <133617549+arushiMWork2004@users.noreply.github.com>
2023-05-29 09:56:39 +1000

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,28 +2,24 @@ import React from 'react';
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex items-center justify-center h-screen w-full bg-red-100">
-      <div className="flex flex-col items-center justify-center h-screen w-2/6">
-        {/* Text */}
-        <div className="text-white text-center">
-          <h2 className="text-5xl font-bold">{heading}</h2>
-          <p className="py-5 text-xl">{message}</p>
-          <button className="px-8 py-2 border">Learn More</button>
-        </div>
-        
-        {/* Lottie Animation */}
-        <div className="p-5">
-          <div>
-            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-            <lottie-player
-              src="https://lottie.host/0c471144-7cb9-4dcf-8b95-f9218bb1e53f/s1Zn31veC6.json"
-              background="transparent"
-              speed="1"
-              style={{ width: '1000px', height: '1000px' }}
-              loop
-              autoplay
-            ></lottie-player>
-          </div>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-red-100">
+      {/* Text */}
+      <div className="text-white text-center">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="py-5 text-xl">{message}</p>
+      </div>
+      {/* Lottie Animation */}
+      <div className="p-5">
+        <div>
+          <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+          <lottie-player
+            src="https://lottie.host/0c471144-7cb9-4dcf-8b95-f9218bb1e53f/s1Zn31veC6.json"
+            background="transparent"
+            speed="1"
+            style={{ width: '100%', maxWidth: '800px', height: 'auto' }}
+            loop
+            autoplay
+          ></lottie-player>
         </div>
       </div>
     </div>
