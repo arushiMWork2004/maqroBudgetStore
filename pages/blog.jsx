9ab3e70f46a2df845d3de3c181abdbe8806f61f8
arushiMWork2004 <133617549+arushiMWork2004@users.noreply.github.com>
2023-05-29 15:31:29 +1000

    Add files via upload

---
+++ pages/blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+    return (
+        <h1>blog</h1>
+    );
+};
+
+export default blog;
---
+++ pages/pricing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pricing = () => {
+    return (
+        <h1>pricing</h1>
+    );
+};
+
+export default pricing;
