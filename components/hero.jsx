f60548891349ab050fc2b048bb8ee995d231fa4a
arushiimittal <111100587+arushiimittal@users.noreply.github.com>
2023-06-01 11:11:56 +1000

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-red-100">
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-red-100 mb-20">
       {/* Text */}
-      <div className="text-white text-center">
-        <h2 className="text-8xl font-bold font-mono sm:text-6xl md:text-7xl lg:text-8xl">{heading}</h2>
-        <p className="py-5 font-bold font-mono text-4xl sm:text-3xl md:text-4xl lg:text-5xl">{message}</p>
-        <div className="mb-4 inline-flex rounded-md shadow">
-          <button className='py-6 px-8 bg-red-200 text-white text-center mr-4 sm:py-4 sm:px-6 sm:text-lg md:py-5 md:px-7 md:text-xl lg:py-6 lg:px-8 lg:text-2xl'>Join Us Now</button>
-          <button className='py-6 px-8 bg-red-400 text-white text-center sm:py-4 sm:px-6 sm:text-lg md:py-5 md:px-7 md:text-xl lg:py-6 lg:px-8 lg:text-2xl'>Request Demo</button>
+      <div className="mt-12">
+        <div className="text-white text-center">
+          <h2 className="text-8xl font-bold font-mono sm:text-6xl md:text-7xl lg:text-8xl">{heading}</h2>
+          <p className="py-5 font-bold font-mono text-4xl sm:text-3xl md:text-4xl lg:text-5xl">{message}</p>
+          <div className="mb-4 inline-flex rounded-md">
+            <button className='py-6 px-8 bg-red-200 text-white text-center mr-4 text-lg sm:py-4 sm:px-6 sm:text-xl md:py-5 md:px-7 md:text-xl lg:py-6 lg:px-8 lg:text-xl xl:py-6 xl:px-8 xl:text-2xl 2xl:py-6 2xl:px-8 2xl:text-3xl'>Join Us Now</button>
+            <button className='py-6 px-8 bg-red-400 text-white text-center sm:py-4 sm:px-6 sm:text-xl md:py-5 md:px-7 md:text-xl lg:py-6 lg:px-8 lg:text-xl xl:py-6 xl:px-8 xl:text-2xl 2xl:py-6 2xl:px-8 2xl:text-3xl'>Request Demo</button>
+          </div>
         </div>
       </div>
       {/* Lottie Animation */}
