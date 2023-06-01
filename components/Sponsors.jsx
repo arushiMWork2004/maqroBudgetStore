e0a5c7f2e46fef4fc71c6b1c9f04181ad9de7592
arushiimittal <111100587+arushiimittal@users.noreply.github.com>
2023-06-01 11:41:33 +1000

    Update Sponsors.jsx

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -2,28 +2,41 @@ import React from 'react';
 
 const SponsorsComponent = () => {
   return (
-    <div className="max-w-screen-xl p-8 mx-auto bg-white">
+    <div className="max-w-screen-xl p-8 mx-auto">
       <h1 className="text-4xl font-bold font-mono text-center p-4">Trusted by</h1>
       <div className="flex flex-wrap justify-center sm:justify-start space-x-4 sm:space-x-8">
         <img
-          src="/download.jpg"
+          src="/download.png"
           alt="Image 1"
-          className="h-64 mb-4 sm:mb-0"
+          className="h-24 mb-4 sm:mb-0"
         />
 
         <img
           src="/download2.png"
           alt="Image 2"
-          className="h-64 mb-4 sm:mb-0"
+          className="h-24 mb-4 sm:mb-0"
         />
 
         <img
           src="/download3.png"
           alt="Image 3"
-          className="h-64 mb-4 sm:mb-0"
+          className="h-24 mb-4 sm:mb-0"
         />
-  
-
+        <img
+          src="/download4.png"
+          alt="Image 4"
+          className="h-24 mb-4 sm:mb-0"
+        />
+        <img
+          src="/download5.png"
+          alt="Image 5"
+          className="h-24 mb-4 sm:mb-0"
+        />
+        <img
+          src="/download6.png"
+          alt="Image 5"
+          className="h-24 mb-4 sm:mb-0"
+        />
         {/* Add more images */}
       </div>
     </div>
