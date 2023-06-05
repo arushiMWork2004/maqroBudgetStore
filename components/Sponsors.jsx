7e238916e626cad48cb33a9a11b15d10a9b6a3e4
arushiimittal <111100587+arushiimittal@users.noreply.github.com>
2023-06-05 12:16:55 +1000

    Update Sponsors.jsx

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -2,42 +2,44 @@ import React from 'react';
 
 const SponsorsComponent = () => {
   return (
-    <div className="max-w-screen-xl p-8 mx-auto mb-24">
-      <h1 className="text-4xl font-bold font-mono text-center p-4">Trusted by</h1>
-      <div className="flex flex-wrap justify-center sm:justify-start space-x-4 sm:space-x-8">
+    <div>
+      <h1 className="text-6xl font-bold font-mono text-center p-4 sm:mb-24 ">Trusted by</h1>
+      <div className="flex justify-center mx-auto mb-24">
+      <div className="flex flex-wrap justify-center space-x-36 sm:justify-start space-x-4">
         <img
           src="/download.png"
           alt="Image 1"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
 
         <img
           src="/download2.png"
           alt="Image 2"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
 
         <img
           src="/download3.png"
           alt="Image 3"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
         <img
           src="/download4.png"
           alt="Image 4"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
         <img
           src="/download5.png"
           alt="Image 5"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
         <img
           src="/download6.png"
           alt="Image 5"
-          className="h-24 mb-4 sm:mb-0"
+          className="h-32 mb-4 sm:mb-0"
         />
         {/* Add more images */}
+      </div>
       </div>
     </div>
   );
