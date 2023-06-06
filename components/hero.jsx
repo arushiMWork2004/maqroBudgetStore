947f409a0c2a154fddf9ee418d596cbc2a239679
arushiimittal <111100587+arushiimittal@users.noreply.github.com>
2023-06-06 11:40:12 +1000

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -4,13 +4,13 @@ const Hero = ({ heading, message }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-red-100 mb-20">
       {/* Text */}
-      <div className="mt-12">
+      <div className="mt-40 mb-20">
         <div className="text-white text-center">
-          <h2 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold font-mono">{heading}</h2>
-          <p className="py-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold font-mono">{message}</p>
+          <h2 className="text-4xl mb-2 sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold font-mono">{heading}</h2>
+          <p className="py-4 text-2xl mb-4 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold font-mono">{message}</p>
           <div className="mb-4 inline-flex rounded-md">
-            <button className='py-4 px-6 sm:py-4 sm:px-6 md:py-5 md:px-7 lg:py-6 lg:px-8 xl:py-6 xl:px-8 2xl:py-6 2xl:px-8 text-lg sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl bg-red-200 text-white text-center mr-4'>Join Us Now</button>
-            <button className='py-4 px-6 sm:py-4 sm:px-6 md:py-5 md:px-7 lg:py-6 lg:px-8 xl:py-6 xl:px-8 2xl:py-6 2xl:px-8 sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl bg-red-400 text-white text-center'>Request Demo</button>
+            <button className='py-4 px-6 sm:py-4 sm:px-6 md:py-5 md:px-7 lg:py-6 lg:px-8 xl:py-6 xl:px-8 2xl:py-6 2xl:px-8 text-lg sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl bg-red-400 text-white text-center mr-4'>Join Us Now</button>
+            <button className='py-4 px-6 sm:py-4 sm:px-6 md:py-5 md:px-7 lg:py-6 lg:px-8 xl:py-6 xl:px-8 2xl:py-6 2xl:px-8 sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl bg-white text-red-400 border border-red-400  text-center'>Request Demo</button>
           </div>
         </div>
       </div>
